d5dfd5047ead07e7b26c473f60a8464e6344fa86
adrian.rodriguez <adrianrr95@gmail.com>
2019-06-29 19:38:46 +0200

    post delivery commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,2 @@
-//export const ROOT_URL = 'http://localhost:4040';
-export const ROOT_URL = 'https://api.arsofthotel.tk';
+export const ROOT_URL = 'http://localhost:4040';
+//export const ROOT_URL = 'https://api.arsofthotel.tk';
